afd25ca0703c629a3a157b19ad53623f4f7e4f40
carlos <carlopolo@gmail.com>
2018-11-23 15:49:30 +0000

    fixing wrong behaviour (at least partially - sometimes there is a hibernate exeption and after retrying it seems to work - )

--- superPortlet/src/main/java/dummyPortlet/portlet/SuperPortlet.java
+++ superPortlet/src/main/java/dummyPortlet/portlet/SuperPortlet.java
@@ -1,9 +1,9 @@
 package dummyPortlet.portlet;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
+import java.util.stream.Collectors;
 
 import javax.portlet.Portlet;
 import javax.portlet.PortletException;
@@ -13,6 +13,7 @@ import org.osgi.service.component.annotations.Component;
 import com.liferay.document.library.kernel.model.DLFolder;
 import com.liferay.document.library.kernel.service.DLFolderLocalServiceUtil;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.model.ResourceAction;
 import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.ResourcePermission;
@@ -43,7 +44,7 @@ import dummyPortlet.constants.DummyPortletKeys;
 public class SuperPortlet extends MVCPortlet {
 	
 	long groupId=20143;
-	long parentFolderId=0;
+	long parentFolderId=35128;
 	long companyId=20116;
 
 	/**
@@ -54,11 +55,12 @@ public class SuperPortlet extends MVCPortlet {
 		super.init();
 		
 		List<DLFolder> folders = getFoldersRecursively(groupId, parentFolderId);
-		List<ResourcePermission> resourcePermisions = ResourcePermissionLocalServiceUtil.getResourcePermissions(companyId,DLFolder.class.getName(),ResourceConstants.SCOPE_INDIVIDUAL,String.valueOf(parentFolderId));
+		List<ResourcePermission> resourcePermissions = ResourcePermissionLocalServiceUtil.getResourcePermissions(companyId,DLFolder.class.getName(),ResourceConstants.SCOPE_INDIVIDUAL,String.valueOf(parentFolderId));
 		folders.forEach(f->{
 				try {
-					System.out.println("\n\n\n"+f.getPath());
-					printFolderPermissions(f);
+					System.out.println(String.format("\n\n\n%s (%s)",f.getPath(),f.getFolderId()));
+					//printFolderPermissions(f);
+					setFolderPermissions(f,resourcePermissions,3);
 				} catch (PortalException e) {
 					e.printStackTrace();
 				}
@@ -71,17 +73,31 @@ public class SuperPortlet extends MVCPortlet {
 	 * @param f
 	 */
 	private void printFolderPermissions(DLFolder f) {
-		List<ResourcePermission> rps = ResourcePermissionLocalServiceUtil.getResourcePermissions(companyId,DLFolder.class.getName(),ResourceConstants.SCOPE_INDIVIDUAL,String.valueOf(f.getFolderId()));
-		rps.forEach(rp->{
-			List<ResourceAction> resourceActions = ResourceActionLocalServiceUtil.getResourceActions(DLFolder.class.getName());
+		List<ResourcePermission> resourcePermissions = ResourcePermissionLocalServiceUtil.getResourcePermissions(companyId,DLFolder.class.getName(),ResourceConstants.SCOPE_INDIVIDUAL,String.valueOf(f.getFolderId()));
+		List<ResourceAction> resourceActions = ResourceActionLocalServiceUtil.getResourceActions(DLFolder.class.getName())
+		.stream().sorted((r1,r2)->Integer.parseInt(""+(r2.getBitwiseValue()-r1.getBitwiseValue()))).collect(Collectors.toList());
+
+		resourcePermissions.forEach(p->{
 			try {
-				long roleId = rp.getRoleId();
-				System.out.print("role="+RoleLocalServiceUtil.getRole(roleId).getName());
+				System.out.print(RoleLocalServiceUtil.getRole(p.getRoleId()).getName());
 			} catch (PortalException e) {
 				e.printStackTrace();
 			}
-			System.out.print(",actionIds="+rp.getActionIds());
-			System.out.print(",resourceActionIds="+resourceActions);
+			ArrayList<Boolean> rlist= new ArrayList<Boolean>();
+			ArrayList<String> nlist= new ArrayList<String>();
+			Iterator<ResourceAction> it = resourceActions.iterator();
+			long aids=p.getActionIds();
+			while(it.hasNext()){
+				ResourceAction current = it.next();
+				if(aids>=current.getBitwiseValue()){
+					aids-=current.getBitwiseValue();
+					rlist.add(true);
+				}else rlist.add(false);
+				nlist.add(current.getActionId());
+			}
+			for(int i=0;i<rlist.size();i++){
+				System.out.print(String.format(",%s=%s", nlist.get(i),rlist.get(i)));
+			}
 			System.out.println("");
 		});
 	}
@@ -89,14 +105,43 @@ public class SuperPortlet extends MVCPortlet {
 	/**
 	 * Propagates the permissions of a folder to the other folders
 	 * @param f
+	 * @throws PortalException 
 	 */
-	private void setFolderPermissions(DLFolder f,List<ResourceAction> resourceActions) {
-		resourceActions.forEach(ra->{
-			ra.getActionId();
-			//ra.s
+	private void setFolderPermissions(DLFolder f,List<ResourcePermission> newResourcePermissions,int attempts) throws PortalException {
+		if (attempts==0)return;
+		System.out.println(String.format("setting roles for %s", f.getPath()));
+		newResourcePermissions.forEach(p->{
+		long actIds = p.getActionIds();
+			try {
+				ResourcePermission currentPermission = ResourcePermissionLocalServiceUtil.getResourcePermission(
+						companyId,
+						DLFolder.class.getName(),
+						ResourceConstants.SCOPE_INDIVIDUAL,
+						""+f.getPrimaryKey(),
+						p.getRoleId());
+				currentPermission.setActionIds(actIds);
+				System.out.print(RoleLocalServiceUtil.getRole(p.getRoleId()).getName());
+				System.out.println(String.format(": before=%s, now=%s", currentPermission.getActionIds(),actIds));
+				ResourcePermissionLocalServiceUtil.updateResourcePermission(currentPermission);
+			} catch (SystemException | PortalException e) {
+				try {
+					System.out.println(String.format("exception for role %s:", RoleLocalServiceUtil.getRole(p.getRoleId()).getName()));
+					ResourcePermission currentPermission = ResourcePermissionLocalServiceUtil.getResourcePermission(
+						companyId,
+						DLFolder.class.getName(),
+						ResourceConstants.SCOPE_INDIVIDUAL,
+						""+f.getPrimaryKey(),
+						p.getRoleId());
+					System.out.println(String.format("old=%s, new=%s", currentPermission.getActionIds(),actIds));
+					Thread.sleep(3000);
+					setFolderPermissions(f, newResourcePermissions, attempts-1);
+				}catch(Exception e2){
+					e.printStackTrace();
+				}
+
+			}
 		});
 	}
-	
 
 	/**
 	 * Returns a list of all sub-folders of a given one
@@ -106,9 +151,8 @@ public class SuperPortlet extends MVCPortlet {
 	 */
 	private List<DLFolder> getFoldersRecursively(long groupId, long parentFolderId){
 		List<DLFolder> folders = DLFolderLocalServiceUtil.getFolders(groupId, parentFolderId);
-		List<DLFolder> result= new ArrayList<DLFolder>();
 		if(folders.isEmpty()) return folders;
-		else
+		List<DLFolder> result= new ArrayList<DLFolder>();
 		folders.forEach(f->{
 			result.add(f);
 			getFoldersRecursively(groupId, f.getFolderId()).forEach(f2->result.add(f2));
